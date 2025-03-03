485065de1379282da78a4d2b7cd2e6e526843673
Richard Marques <richardbh42@hotmail.com>
2025-03-03 00:38:17 -0300

    primeiro teste automatizado

---
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -0,0 +1,14 @@
+/// <reference types="cypress"/>
+
+describe('Funcionalidade: Login', () => {
+
+    it('Deve fazer login com sucesso', () => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('#username').type('richardteste@gmail.com')
+        cy.get('#password').type('R36255909')
+        cy.get('.woocommerce-form > .button').click()
+
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain','Olá, richardteste-6671 (não é richardteste-6671? Sair)')
+    })
+
+})
